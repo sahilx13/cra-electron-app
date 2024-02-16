c90bb2e4677b83b55d4f72822c5c86b35bf290a2
Sahil Sharma <sahilsharma@umass.edu>
2024-02-15 21:07:05 -0500

    add file path functions to the app

--- preload.js
+++ preload.js
@@ -1,10 +1,17 @@
-window.addEventListener('DOMContentLoaded', () => {
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  openFile: () => ipcRenderer.invoke("dialog:openFile"),
+  openFolder: () => ipcRenderer.invoke("dialog:openDirectory"),
+});
+
+window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
   };
 
-  for (const dependency of ['chrome', 'node', 'electron']) {
+  for (const dependency of ["chrome", "node", "electron"]) {
     replaceText(`${dependency}-version`, process.versions[dependency]);
   }
 });
